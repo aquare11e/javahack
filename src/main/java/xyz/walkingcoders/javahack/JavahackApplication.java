28419e9c8926de6a35d8b11cfc802735bfbe2ffb
Ruslan Komarov <aquare11e@yandex.ru>
2019-09-13 19:44:37 +0300

    Initialized Java project

---
+++ src/main/java/xyz/walkingcoders/javahack/JavahackApplication.java
@@ -0,0 +1,13 @@
+package xyz.walkingcoders.javahack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavahackApplication.class, args);
+	}
+
+}
