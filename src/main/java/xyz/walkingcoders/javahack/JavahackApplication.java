2d48baf41dbf71d5e880c5441617f55bbb92dd59
Ruslan Komarov <aquare11e@yandex.ru>
2019-09-13 23:43:33 +0300

    Added test request

--- src/main/java/xyz/walkingcoders/javahack/JavahackApplication.java
+++ src/main/java/xyz/walkingcoders/javahack/JavahackApplication.java
@@ -2,6 +2,7 @@ package xyz.walkingcoders.javahack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavahackApplication {
@@ -10,4 +11,8 @@ public class JavahackApplication {
 		SpringApplication.run(JavahackApplication.class, args);
 	}
 
+	@GetMapping("/testRequest")
+	public String testRequest() {
+		return "Ok";
+	}
 }
