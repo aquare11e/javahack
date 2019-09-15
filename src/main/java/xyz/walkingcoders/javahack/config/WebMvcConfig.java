ccc690ce1bb7212a58fbaf308df07c5638224ce0
Ruslan Komarov <aquare11e@yandex.ru>
2019-09-15 13:15:02 +0300

    Update DB setting and other stuff

--- src/main/java/xyz/walkingcoders/javahack/config/WebMvcConfig.java
+++ src/main/java/xyz/walkingcoders/javahack/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package xyz.walkingcoders.javahack.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.walkingcoders.javahack.utils.IdHttpHeaderInterceptor;
@@ -15,5 +16,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(idHttpHeaderInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
