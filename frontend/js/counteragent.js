52e0012b3cf4669203a30485f1df2585c32662a1
Boroda76 <samborod76@gmail.com>
2019-09-15 11:22:03 +0300

    front check agent done

--- frontend/js/counteragent.js
+++ frontend/js/counteragent.js
@@ -19,8 +19,8 @@ function fillIndividual(data) {
     $('#inn_form').hide();
     $('.table').append('<tbody><tr><td>Наименование ЮЛ</td><td>'+data['egr']['items'][0]['ИП']['ФИОПолн']+'</td></tr>' +
         '<tr><td>Статус</td><td>'+data['egr']['items'][0]['ИП']['Статус']+'</td></tr>' +
-        '<tr><td>Позитивные факторы</td><td>'+data['check']['items'][0]['ИП']['Позитив']['Текст']+'</td></tr>'+
-        '<tr><td>Негативные факторы</td><td>'+data['check']['items'][0]['ИП']['Негатив']['Текст']+'</td></tr></tbody>');
+        '<tr><td>Позитивные факторы</td><td>'+(data['check']['items'][0]['ИП']['Позитив']['Текст']==null?'':data['check']['items'][0]['ИП']['Позитив']['Текст'])+'</td></tr>'+
+        '<tr><td>Негативные факторы</td><td>'+(data['check']['items'][0]['ИП']['Негатив']['Текст']==null?'':data['check']['items'][0]['ИП']['Негатив']['Текст'])+'</td></tr></tbody>');
     $('#check_image').append('<img src="img/'+(data['check']['items'][0]['ИП']['Позитив']['Текст'].length>0?data['check']['items'][0]['ИП']['Негатив']['Текст'].length>0?'well':'good':'bad')+'.svg">')
 }
 
@@ -29,8 +29,8 @@ function fillLegal(data) {
     $('#inn_form').hide();
     $('.table').append('<tbody><tr><td>Наименование ЮЛ</td><td>'+data['egr']['items'][0]['ЮЛ']['НаимПолнЮЛ']+'</td></tr>' +
         '<tr><td>Статус</td><td>'+data['egr']['items'][0]['ЮЛ']['Статус']+'</td></tr>' +
-        '<tr><td>Позитивные факторы</td><td>'+data['check']['items'][0]['ЮЛ']['Позитив']['Текст']+'</td></tr>'+
-        '<tr><td>Негативные факторы</td><td>'+data['check']['items'][0]['ЮЛ']['Негатив']['Текст']+'</td></tr></tbody>');
+        '<tr><td>Позитивные факторы</td><td>'+(data['check']['items'][0]['ЮЛ']['Позитив']['Текст']==null?'':data['check']['items'][0]['ЮЛ']['Позитив']['Текст'])+'</td></tr>'+
+        '<tr><td>Негативные факторы</td><td>'+(data['check']['items'][0]['ЮЛ']['Негатив']['Текст']==null?'':data['check']['items'][0]['ЮЛ']['Негатив']['Текст'])+'</td></tr></tbody>');
     $('#check_image').append('<img src="img/'+(data['check']['items'][0]['ЮЛ']['Позитив']['Текст'].length>0?data['check']['items'][0]['ЮЛ']['Негатив']['Текст'].length>0?'well':'good':'bad')+'.svg">')
 
 }
